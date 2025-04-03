0a586e6e42f8d63117900d61487a0762d1eaf5d1
UBSITDocadoc <20235473@s.ubaguio.edu>
2025-04-03 15:21:09 +0800

    pushing created components

--- StudentResourceHub/src/app/app.module.ts
+++ StudentResourceHub/src/app/app.module.ts
@@ -7,6 +7,12 @@ import { HeaderComponent } from './header/header.component';
 import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { AboutComponent } from './about/about.component';
+import { AddResourceComponent } from './add-resource/add-resource.component';
+import { ProgrammingComponent } from './Categories/programming/programming.component';
+import { DesignComponent } from './Categories/design/design.component';
+import { MathComponent } from './Categories/math/math.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +20,13 @@ import { HomeComponent } from './home/home.component';
     HeaderComponent,
     NavigationMenuComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    CategoriesComponent,
+    AboutComponent,
+    AddResourceComponent,
+    ProgrammingComponent,
+    DesignComponent,
+    MathComponent
   ],
   imports: [
     BrowserModule,
---
+++ StudentResourceHub/src/app/categories/programming/programming.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-programming',
+  standalone: false,
+  templateUrl: './programming.component.html',
+  styleUrl: './programming.component.css'
+})
+export class ProgrammingComponent {
+
+}
